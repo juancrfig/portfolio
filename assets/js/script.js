c989ebcf39bf87cd17ccab97131751a59794e0f4
juancrfig <juancrfig@gmail.com>
2024-12-16 16:24:35 -0500

    Add quote functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,5 +32,38 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.1 // Triggers when 10% of the element is visible
+  });
+  
+  // Select all elements you want to animate
+  document.querySelectorAll('.animate-on-scroll').forEach(el => {
+    observer.observe(el);
+  });
 
+// ----------------------------------------------------------
+// QUOTE GENERATOR 
+// ----------------------------------------------------------
 
+const quotes = [
+    {
+    "quote": "If I have seen further than others it is by standing upon the shoulders of giants",
+    "author": "Isaac Newton" 
+    },
+    {"quote": "Without training, they lacked knowledge. Without knowledge, they lacked confidence. Without confidence, they lacked victory",
+    "author": "Julius Caesar"
+    },
+]
+
+const quoteElement = document.querySelector('#quote');
+const authorElement = document.querySelector('#author');
+const randomNumber = Math.floor(Math.random() * quotes.length)
+
+quoteElement.textContent = quotes[randomNumber].quote;
+authorElement.textContent = quotes[randomNumber].author;
