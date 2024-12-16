ea0af27540b3d3a0a8e8814156f0d3ceec354d1c
juancrfig <juancrfig@gmail.com>
2024-12-16 15:16:15 -0500

    Finish welcome section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const navbar = document.querySelector('nav');
-
+const workingElement = document.querySelector('.card a')
 window.addEventListener('scroll', () => {
 
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -12,3 +12,25 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('minimized');
     }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const underlineContainers = document.querySelectorAll('.underline-container');
+    
+    underlineContainers.forEach((container) => {
+        const span = container.querySelector('span');
+        const underline = container.querySelector('.underline-effect');
+        
+        setTimeout(() => {
+            // Measure the width of the span to create a precise underline
+            const spanWidth = span.offsetWidth;
+            underline.style.width = `${spanWidth}px`;
+        }, 500); // Stagger the animations
+
+        setTimeout(() => {
+            workingElement.classList.add('color-butterscotch')
+        }, 1000)
+    });
+});
+
+
+
