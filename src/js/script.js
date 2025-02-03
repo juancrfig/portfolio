9a15d1f7f4109ace79e1fe60753f1cd72c3e59ab
juancrfig <juancrfig@gmail.com>
2025-02-03 14:04:38 -0500

    Improve color github icon

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,11 @@
-const typingSpeed = 60; // Typing speed in milliseconds
+const inputTheme = document.querySelector('#input-theme');
+const gitImgContainer = document.querySelector('#github-img-container');
 
-function typeEffect(element) {
 
-  const text = element.getAttribute("data-text"); // Get the text to type
-  let index = 0; // Start typing from the first character
-  function type() {
-    if (index < text.length) {
-      element.textContent += text[index];
-      index++;
-      setTimeout(type, typingSpeed);
-    }
-  }
-  type(); // Start the typing effect
-}
-// Apply the effect to all elements with the class "type-effect"
-function applyTypingEffectToAll() {
-  const elements = document.querySelectorAll(".type-effect");
-  elements.forEach((element) => {
-    typeEffect(element);
-  });
-}
-// Trigger the typing effect on window load
-window.onload = applyTypingEffectToAll;
+inputTheme.addEventListener('click', () => {
+	if (inputTheme.checked) {
+		gitImgContainer.setAttribute('src', './src/media/icon-git-dark.svg');
+	} else {
+		gitImgContainer.setAttribute('src', './src/media/icon-github.svg');
+	}
+})
